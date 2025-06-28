6186cf1eba96a58ea5f32a2321a0c64a3e36b421
Abir Hosen <abir71.hosen@gmail.com>
2025-06-28 17:19:58 +0600

    update content

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
               Specializing in software development, media production, and AI-powered solutions
               to help businesses grow and innovate.
             </p>
-            <div className="mt-6 flex space-x-4">
+            {/* <div className="mt-6 flex space-x-4">
               <a href="#" className="text-gray-400 hover:text-white">
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
@@ -42,7 +42,7 @@ const Footer = () => {
                   <path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clipRule="evenodd" />
                 </svg>
               </a>
-            </div>
+            </div> */}
           </div>
 
           {/* Quick Links */}
@@ -79,15 +79,15 @@ const Footer = () => {
             <ul className="space-y-4">
               <li className="flex items-start">
                 <MapPin className="h-5 w-5 text-blue-500 mt-1 mr-3" />
-                <span className="text-gray-400">123 Innovation Street, Tech City, 10001</span>
+                <span className="text-gray-400">Dhaka - 1214, Bangladesh</span>
               </li>
               <li className="flex items-center">
                 <Phone className="h-5 w-5 text-blue-500 mr-3" />
-                <span className="text-gray-400">+1 (555) 123-4567</span>
+                <span className="text-gray-400">+880 1323 987 404</span>
               </li>
               <li className="flex items-center">
                 <Mail className="h-5 w-5 text-blue-500 mr-3" />
-                <a href="mailto:info@cryptdox.com" className="text-gray-400 hover:text-white transition-colors">info@cryptdox.com</a>
+                <a href="mailto:info@cryptdox.com" className="text-gray-400 hover:text-white transition-colors">cryptdox.ethos@gmail.com</a>
               </li>
             </ul>
           </div>
